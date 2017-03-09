f3e562efb95af5fe0b43409316b7c1fa328a8c99
omaotzu <omarhphillips@gmail.com>
2017-03-09 21:37:02 +0000

    Updated config

--- config/environment.js
+++ config/environment.js
@@ -2,5 +2,5 @@ const port = process.env.PORT || 3000;
 const env = process.env.NODE_ENV ||'development';
 const dbURI = process.env.MONGODB_URI || `mongodb://localhost/project-2-${env}`;
 const sessionSecret = process.env.SESSION_SECRET || 'my awesome secret';
-const wunderground = process.env.WUNDERGROUND_ACCESS_KEY || 'development';
-module.exports = { port, env, dbURI, sessionSecret, wunderground};
+const url = env === 'development' ? 'http://localhost:3000':'https://enigmatic-meadow-18873.herokuapp.com';
+module.exports = { port, env, dbURI, sessionSecret, url};
